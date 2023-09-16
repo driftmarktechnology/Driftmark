a29e398173bd762dbe98eb43cfe101ef7fcc51f8
Anbu <anbu@Anbus-MacBook-Air.local>
2023-09-16 18:57:40 +0530

    added tech and blogs page

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,143 +1,150 @@
 import React from "react";
+import Confetti from "react-confetti";
 
 function Team() {
-  return (
-    <section id="team" class="team section-bg">
-      <div class="container">
-        <div class="section-title">
-          <span>Team</span>
-          <h2>Team</h2>
-          <h1>Transforming Business Concepts into Reality.</h1>
-          <p>
-            At Driftmark, we've honed our expertise in transforming innovative
-            concepts into impactful products for our partners. Many of the
-            projects we're most proud of began as budding ideas and evolved into
-            thriving businesses from scratch. Witnessing our partners dominate
-            their market segments with our digital solutions fills us with
-            immense pride.
-          </p>
-          <p>
-            While we typically adhere to a structured process to ensure every
-            concept is nurtured to evolve into a unique product, we recognize
-            the distinctiveness of each project at Driftmark. We're flexible in
-            our approach, understanding that not all projects fit a mold. We're
-            always open to refining our methods and are ready to revisit the
-            foundational stages whenever necessary.
-          </p>
-        </div>
+  let width = window.innerWidth;
+  let height = window.innerHeight;
 
+  return (
+    <>
+      <Confetti width={width} height={height} />
+      <section id="team" class="team section-bg">
         <div class="container">
           <div class="section-title">
-            <span>Meet Our Experts for You</span>
-            <h2>Meet Our Experts for You</h2>
-            <div class="row">
-              <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
-                <div class="member">
-                  <img src="/assets/img/suji.jpg" alt="" height="313" />
-                  <h4>Kesavan Sujitha</h4>
-                  <span>Director</span>
-                  <p>
-                    Voluptas necessitatibus occaecati quia. Earum totam
-                    consequuntur qui porro et laborum toro des clara
-                  </p>
-                  <div class="social">
-                    <a href="">
-                      <i class="bi bi-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-linkedin"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
-                <div class="member">
-                  <img src="/assets/img/naveen.jpg" alt="" />
-                  <h4>Murugesan Naveen</h4>
-                  <span>Director</span>
-                  <p>
-                    Magni qui quod omnis unde et eos fuga et exercitationem.
-                    Odio veritatis perspiciatis quaerat qui aut aut aut
-                  </p>
-                  <div class="social">
-                    <a href="">
-                      <i class="bi bi-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-linkedin"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
+            <span>Team</span>
+            <h2>Team</h2>
+            <h1>Transforming Business Concepts into Reality.</h1>
+            <p>
+              At Driftmark, we've honed our expertise in transforming innovative
+              concepts into impactful products for our partners. Many of the
+              projects we're most proud of began as budding ideas and evolved
+              into thriving businesses from scratch. Witnessing our partners
+              dominate their market segments with our digital solutions fills us
+              with immense pride.
+            </p>
+            <p>
+              While we typically adhere to a structured process to ensure every
+              concept is nurtured to evolve into a unique product, we recognize
+              the distinctiveness of each project at Driftmark. We're flexible
+              in our approach, understanding that not all projects fit a mold.
+              We're always open to refining our methods and are ready to revisit
+              the foundational stages whenever necessary.
+            </p>
+          </div>
 
-              <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
-                <div class="member">
-                  <img src="assets/img/team/profile.jpg" alt="" />
-                  <h4>Member</h4>
-                  <span>Designation</span>
-                  <p>
-                    Voluptas necessitatibus occaecati quia. Earum totam
-                    consequuntur qui porro et laborum toro des clara
-                  </p>
-                  <div class="social">
-                    <a href="">
-                      <i class="bi bi-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-linkedin"></i>
-                    </a>
+          <div class="container">
+            <div class="section-title">
+              <span>Meet Our Experts for You</span>
+              <h2>Meet Our Experts for You</h2>
+              <div class="row">
+                <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
+                  <div class="member">
+                    <img src="/assets/img/suji.jpg" alt="" height="313" />
+                    <h4>Kesavan Sujitha</h4>
+                    <span>Director</span>
+                    <p>
+                      Voluptas necessitatibus occaecati quia. Earum totam
+                      consequuntur qui porro et laborum toro des clara
+                    </p>
+                    <div class="social">
+                      <a href="">
+                        <i class="bi bi-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-instagram"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-linkedin"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
+                <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
+                  <div class="member">
+                    <img src="/assets/img/naveen.jpg" alt="" />
+                    <h4>Murugesan Naveen</h4>
+                    <span>Director</span>
+                    <p>
+                      Magni qui quod omnis unde et eos fuga et exercitationem.
+                      Odio veritatis perspiciatis quaerat qui aut aut aut
+                    </p>
+                    <div class="social">
+                      <a href="">
+                        <i class="bi bi-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-instagram"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-linkedin"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
 
-              <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
-                <div class="member">
-                  <img src="assets/img/team/profile.jpg" alt="" />
-                  <h4>Member</h4>
-                  <span>Designation</span>
-                  <p>
-                    Voluptas necessitatibus occaecati quia. Earum totam
-                    consequuntur qui porro et laborum toro des clara
-                  </p>
-                  <div class="social">
-                    <a href="">
-                      <i class="bi bi-twitter"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-facebook"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-instagram"></i>
-                    </a>
-                    <a href="">
-                      <i class="bi bi-linkedin"></i>
-                    </a>
+                <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
+                  <div class="member">
+                    <img src="assets/img/team/profile.jpg" alt="" />
+                    <h4>Member</h4>
+                    <span>Designation</span>
+                    <p>
+                      Voluptas necessitatibus occaecati quia. Earum totam
+                      consequuntur qui porro et laborum toro des clara
+                    </p>
+                    <div class="social">
+                      <a href="">
+                        <i class="bi bi-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-instagram"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-linkedin"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-3 d-flex align-items-stretch">
+                  <div class="member">
+                    <img src="assets/img/team/profile.jpg" alt="" />
+                    <h4>Member</h4>
+                    <span>Designation</span>
+                    <p>
+                      Voluptas necessitatibus occaecati quia. Earum totam
+                      consequuntur qui porro et laborum toro des clara
+                    </p>
+                    <div class="social">
+                      <a href="">
+                        <i class="bi bi-twitter"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-facebook"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-instagram"></i>
+                      </a>
+                      <a href="">
+                        <i class="bi bi-linkedin"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
